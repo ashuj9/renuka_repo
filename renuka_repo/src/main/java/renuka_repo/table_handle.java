061119a78c58f8664b0845644da18a30cbfe7b1d
ashuj9 <138884875+ashuj9@users.noreply.github.com>
2023-07-14 14:16:50 +0530

    done

---
+++ renuka_repo/src/main/java/renuka_repo/table_handle.java
@@ -0,0 +1,22 @@
+package renuka_repo;
+
+import java.time.Duration;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class table_handle {
+
+	public static void main(String[] args) {
+		{
+			WebDriver d=new ChromeDriver();
+			d.manage().window().maximize();
+			d.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+			d.get("https://www.seleniumpractise.blogspot.com");
+			
+
+	}
+
+}
+
