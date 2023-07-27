dae4c35e516e4793387f8c3085f8124515d4bd1c
ashuj9 <138884875+ashuj9@users.noreply.github.com>
2023-07-27 18:52:12 +0530

    tc_fb

---
+++ cucumber/src/test/java/cucumber/facebook.java
@@ -0,0 +1,16 @@
+package cucumber;
+
+import io.cucumber.java.en.Then;
+
+public class facebook {
+	@Then("facebook login page is opened sucessfully")
+	public void facebook_login_page_is_opened_sucessfully() {
+	    
+	}
+
+
+
+	
+	
+
+}
